55f13a5b94a5e99c5a0dcbf46d6e412079601f12
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-06-27 10:39:41 +0000

    27/06/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Hello world!");
-
+    System.out.println("vamos identificar se o número é positivo ou negativo");
+    System.out.println("");
+    System.out.println("digite um numero inteiro ");
+    double numero = sc.nextDouble();
+   
+if( numero>0) {
+    System.out.println("o numero é positivo");
+}
+    
+  if(numero<0){
+    System.out.println("o numero e negativo");
+  }  
 
 
 
